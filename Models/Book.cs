ed6530a67e4bc3873e51826a002506d0901d921c
João Lucas <joaolucasfreitaslul@gmail.com>
2026-03-02 22:45:47 -0400

    created book model

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivrariaPlus.Api.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Author { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
